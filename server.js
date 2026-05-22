fac7c5a07d2ddc1a6d4f6a7c6e7800453c0ed6cd
sami10-byte <ahblsami10@gmail.com>
2026-05-22 20:03:49 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.send(`
   <!DOCTYPE html>
   <html lang="ar" dir="rtl">
